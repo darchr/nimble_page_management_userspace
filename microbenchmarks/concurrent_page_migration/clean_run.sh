35ce9dbd81118288d8483b486b4d3014e56fc9b4
JulianToya <jtangeles@ucdavis.edu>
2021-08-05 17:36:36 -0700

    Add filest for resetting runs

---
+++ microbenchmarks/concurrent_page_migration/clean_run.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+rm -rf dram_non_thp_verify_4kb/*
+rm -rf dram_stats_2mb/*
+rm -rf dram_stats_4kb/*
+rm -rf dram_thp_verify_2mb/*
+rm -rf nv_non_thp_verify_4kb/*
+rm -rf nv_stats_2mb/*
+rm -rf nv_stats_4kb/*
+rm -rf nv_thp_verify_2mb/*
